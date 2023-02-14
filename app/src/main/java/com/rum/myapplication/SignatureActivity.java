686df13775fcdea5a05d7538ee0b482562dea97f
RumitPatel <rumit.patel5@gmail.com>
2023-02-14 15:25:24 +0530

    videoview set and played.

--- app/src/main/java/com/rum/myapplication/SignatureActivity.java
+++ app/src/main/java/com/rum/myapplication/SignatureActivity.java
@@ -2,7 +2,10 @@ package com.rum.myapplication;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -15,8 +18,8 @@ public class SignatureActivity extends AppCompatActivity {
 
     private Context mContext;
     private ActivitySignatureBinding binding;
-    private DrawingView drawingView;
-    private boolean isImageSaved = false;
+
+    private boolean isPaintEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +27,59 @@ public class SignatureActivity extends AppCompatActivity {
         mContext = this;
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signature);
 
+        setListeners();
         initDrawingView();
+        initAndPlayVideo();
 
+        hidePaint();
+    }
+
+    private void setListeners() {
+        binding.btnStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPaintEnabled) {
+                    hidePaint();
+                } else {
+                    showPaintOption();
+                }
+            }
+        });
     }
 
     private void initDrawingView() {
         Paint mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mPaint.setColor(ContextCompat.getColor(mContext, R.color.colorBlack));
+        mPaint.setColor(ContextCompat.getColor(mContext, R.color.colorOrange));
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setStrokeWidth(8);
-        drawingView = new DrawingView(mContext, mPaint);
+        DrawingView drawingView = new DrawingView(mContext, mPaint);
 
         binding.llSignatureView.addView(drawingView);
         binding.llSignatureView.invalidate();
+    }
+
+    private void initAndPlayVideo() {
+        MediaController mediaController= new MediaController(mContext);
+        mediaController.setAnchorView(binding.videoView);
+        Uri uri=Uri.parse("http://techslides.com/demos/sample-videos/small.mp4");
+        binding.videoView.setMediaController(mediaController);
+        binding.videoView.setVideoURI(uri);
+        binding.videoView.requestFocus();
+
+        binding.videoView.start();
+    }
+
+    private void showPaintOption() {
+        isPaintEnabled = true;
+        binding.llSignatureView.setVisibility(View.VISIBLE);
+    }
+
+    private void hidePaint() {
+        isPaintEnabled = false;
+        binding.llSignatureView.setVisibility(View.GONE);
     }
 }
