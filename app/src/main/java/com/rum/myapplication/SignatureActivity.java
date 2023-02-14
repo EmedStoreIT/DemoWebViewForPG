c14277b4099c1ec10f4811df58f8791e3f16c469
RumitPatel <rumit.patel5@gmail.com>
2023-02-14 14:35:39 +0530

    save and clear button removed.

--- app/src/main/java/com/rum/myapplication/SignatureActivity.java
+++ app/src/main/java/com/rum/myapplication/SignatureActivity.java
@@ -1,12 +1,8 @@
 package com.rum.myapplication;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -14,16 +10,12 @@ import androidx.databinding.DataBindingUtil;
 
 import com.rum.myapplication.databinding.ActivitySignatureBinding;
 import com.rum.myapplication.generalHelper.DrawingView;
-import com.rum.myapplication.generalHelper.L;
-import com.rum.myapplication.generalHelper.SP;
 
-import java.io.File;
-import java.io.FileOutputStream;
+public class SignatureActivity extends AppCompatActivity {
 
-public class SignatureActivity extends AppCompatActivity implements View.OnClickListener {
-    private DrawingView drawingView;
-    private ActivitySignatureBinding binding;
     private Context mContext;
+    private ActivitySignatureBinding binding;
+    private DrawingView drawingView;
     private boolean isImageSaved = false;
 
     @Override
@@ -37,9 +29,6 @@ public class SignatureActivity extends AppCompatActivity implements View.OnClick
 
         binding.llSignatureView.addView(drawingView);
         binding.llSignatureView.invalidate();
-//
-        binding.btnSaveSignature.setOnClickListener(this);
-        binding.btnClearSignature.setOnClickListener(this);
     }
 
     private void initDrawingView() {
@@ -52,72 +41,5 @@ public class SignatureActivity extends AppCompatActivity implements View.OnClick
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setStrokeWidth(8);
         drawingView = new DrawingView(mContext, mPaint);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnSaveSignature:
-                saveSignature();
-                break;
-            case R.id.btnClearSignature:
-                SP.savePreferences(mContext, SP.SIGNATURE_IMAGE_PATH, "");
-                drawingView.clear();
-                drawingView.invalidate();
-                break;
-        }
-    }
-
-    public void saveSignature() {
-        if (!DrawingView.haveDrawSomething) {
-            L.showToast(mContext, getString(R.string.take_valid_signature));
-            return;
-        }
-
-        binding.llSignatureView.setDrawingCacheEnabled(true);
-        binding.llSignatureView.buildDrawingCache(true);
-        Bitmap bitmap = binding.llSignatureView.getDrawingCache();
-
-        File pictureFile = getOutputMediaFile();
-        if (pictureFile == null) {
-            L.showToast(mContext, getString(R.string.error_saving_signature));
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
-            fos.close();
-            isImageSaved = true;
-        } catch (Exception e) {
-            isImageSaved = false;
-        } finally {
-            binding.llSignatureView.setDrawingCacheEnabled(false);
-            binding.llSignatureView.buildDrawingCache(false);
-            drawingView.clear();
-            drawingView.invalidate();
-
-            if (isImageSaved) {
-                SP.savePreferences(mContext, SP.SIGNATURE_IMAGE_PATH, pictureFile.toString());
-                L.showToast(mContext, getString(R.string.signatur_saved));
-                Intent intent = new Intent();
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            } else {
-                L.showToast(mContext, getString(R.string.error_saving_signature));
-            }
-        }
-    }
-
-    private File getOutputMediaFile() {
-        File mediaStorageDir = new File(getExternalFilesDir(null).toString() + "/" + getString(R.string.app_name));
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                L.showToast(mContext, getString(R.string.error_saving_signature));
-            }
-        }
-        File mediaFile;
-        String mImageName = "SIGN_" + System.currentTimeMillis() + ".png";
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
-        return mediaFile;
     }
 }
