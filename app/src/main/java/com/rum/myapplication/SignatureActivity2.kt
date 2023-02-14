8ab562b388bf7af914f045c33e957fe851067757
RumitPatel <rumit.patel5@gmail.com>
2023-02-14 17:30:13 +0530

    working on exo_player

---
+++ app/src/main/java/com/rum/myapplication/SignatureActivity2.kt
@@ -0,0 +1,131 @@
+package com.rum.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Paint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import com.rum.myapplication.databinding.ActivitySignatureBinding
+import com.rum.myapplication.generalHelper.DrawingView
+
+@UnstableApi
+class SignatureActivity2 : AppCompatActivity() {
+    private lateinit var mContext: Context
+    private lateinit var binding: ActivitySignatureBinding
+
+    private var isPaintEnabled = false
+
+    private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+        binding = ActivitySignatureBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+        setListeners()
+    }
+
+    private fun initComponents() {
+        hideProgress()
+        hidePaint()
+        initDrawingView()
+    }
+
+    private fun setListeners() {
+        binding.btnStartStop.setOnClickListener { view ->
+            if (isPaintEnabled) {
+                hidePaint()
+            } else {
+                showPaintOption()
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun initDrawingView() {
+        val mPaint = Paint()
+        mPaint.isAntiAlias = true
+        mPaint.isDither = true
+        mPaint.color = ContextCompat.getColor(mContext, R.color.colorOrange)
+        mPaint.style = Paint.Style.STROKE
+        mPaint.strokeJoin = Paint.Join.ROUND
+        mPaint.strokeCap = Paint.Cap.ROUND
+        mPaint.strokeWidth = 8f
+        val drawingView = DrawingView(mContext, mPaint)
+        binding.llSignatureView.addView(drawingView)
+        binding.llSignatureView.invalidate()
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                binding.videoView.player = exoPlayer
+
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                exoPlayer.setMediaItem(mediaItem)
+
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.prepare()
+
+            }
+    }
+
+    private fun showPaintOption() {
+        isPaintEnabled = true
+        binding.llSignatureView.visibility = View.VISIBLE
+    }
+
+    private fun hidePaint() {
+        isPaintEnabled = false
+        binding.llSignatureView.visibility = View.GONE
+    }
+
+    private fun hideProgress() {
+        binding.progressBar.visibility = View.GONE
+    }
+
+    private fun showProgress() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+}
